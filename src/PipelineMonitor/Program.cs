cdb57a4e823534bd268d10ea88c33971a6a5792f
Logan Bussell <loganbussell@microsoft.com>
2026-01-16 11:48:42 -0800

    Add CLI interface (#14)

--- src/PipelineMonitor/Program.cs
+++ src/PipelineMonitor/Program.cs
@@ -1,14 +1,16 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026 Logan Bussell
 // SPDX-License-Identifier: MIT
 
+using ConsoleAppFramework;
+
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using PipelineMonitor;
 using PipelineMonitor.AzureDevOps;
 using PipelineMonitor.Logging;
+
 using Spectre.Console;
 
 var builder = Host.CreateApplicationBuilder();
@@ -16,78 +18,78 @@ builder.Configuration.SetBasePath(AppContext.BaseDirectory);
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
 
 builder.Services.TryAddPipelinesService();
-builder.Services.TryAddOrganizationDiscoveryService();
-builder.Services.TryAddRepoInfoResolver();
 builder.Services.TryAddInteractionService();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddFileLogger(builder.Configuration);
 builder.Logging.AddLogLocationOnExit();
 
-var host = builder.Build();
+var consoleAppBuilder = builder.ToConsoleAppBuilder();
+consoleAppBuilder.Add<App>();
 
-var applicationLifetimeTokenSource = new CancellationTokenSource();
-var runTask = host.RunAsync(applicationLifetimeTokenSource.Token);
-
-var orgExample = async () =>
-{
-    var orgService = host.Services.GetRequiredService<IOrganizationDiscoveryService>();
-    var organizations = await orgService.GetAccessibleOrganizationsAsync();
-    Console.WriteLine("Accessible Organizations:");
-    foreach (var org in organizations) Console.WriteLine($"- {org}");
-};
-
-var pipelineExample = async () =>
-{
-    var pipelinesService = host.Services.GetRequiredService<PipelinesService>();
-    var pipeline = await pipelinesService.GetPipelineAsync(
-        new OrganizationInfo("dnceng", new Uri("https://dev.azure.com/dnceng")),
-        new ProjectInfo("internal"),
-        new PipelineId(1434));
-    Console.WriteLine(pipeline);
-};
-
-var allPipelinesExample = async () =>
-{
-    var pipelinesService = host.Services.GetRequiredService<PipelinesService>();
-    var allPipelines = pipelinesService.GetAllPipelinesAsync(
-        new OrganizationInfo("dnceng", new Uri("https://dev.azure.com/dnceng")),
-        new ProjectInfo("internal"));
-    await foreach (var pipeline in allPipelines) Console.WriteLine(pipeline);
-};
-
-var repoInfoExample = async () =>
-{
-    var resolver = host.Services.GetRequiredService<IRepoInfoResolver>();
-    var info = await resolver.ResolveAsync();
-    Console.WriteLine($"Detected Repository: {info}");
-};
-
-var localPipelinesExample = async () =>
-{
-    var pipelinesService = host.Services.GetRequiredService<PipelinesService>();
-    var interactionService = host.Services.GetRequiredService<IInteractionService>();
-    var ansiConsole = host.Services.GetRequiredService<IAnsiConsole>();
-
-    var pipelines = await interactionService.ShowStatusAsync(
-        "Loading pipelines...",
-        async () => await pipelinesService.GetLocalPipelinesAsync().ToListAsync());
-
-    var table = new Table()
-        .Border(TableBorder.Simple)
-        .AddColumn("Definition")
-        .AddColumn("Pipeline");
-
-    foreach (var pipeline in pipelines)
-        table.AddRow($"[blue]{pipeline.RelativePath}[/]", $"[bold green]{pipeline.Name}[/]");
-
-    ansiConsole.Write(table);
-};
-
-await localPipelinesExample();
-
-// Now stuff is done.
-// Signal to stop the application.
-applicationLifetimeTokenSource.Cancel();
-// And then wait for services to gracefully shut down.
+var runTask = consoleAppBuilder.RunAsync(args);
 await runTask;
+
+internal sealed class App(
+    IAnsiConsole ansiConsole,
+    IInteractionService interactionService)
+{
+    private readonly IAnsiConsole _ansiConsole = ansiConsole;
+    private readonly IInteractionService _interactionService = interactionService;
+
+    [Command("discover")]
+    public async Task DiscoverPipelinesAsync([FromServices] PipelinesService pipelinesService)
+    {
+        var pipelinesTask = pipelinesService
+            .GetLocalPipelinesAsync()
+            .ToListAsync()
+            .AsTask();
+
+        IReadOnlyList<LocalPipelineInfo> pipelines = await _interactionService
+            .ShowStatusAsync("Loading pipelines...", () => pipelinesTask);
+
+        var table = new Table()
+            .Border(TableBorder.Simple)
+            .AddColumn("Definition")
+            .AddColumn("Pipeline");
+
+        foreach (var pipeline in pipelines)
+            table.AddRow(
+                $"[blue]{pipeline.RelativePath}[/]",
+                $"[bold green]{pipeline.Name}[/]");
+
+        _ansiConsole.Write(table);
+    }
+
+    [Command("info")]
+    public async Task ShowPipelineInfoAsync(
+        [FromServices] PipelinesService pipelinesService,
+        [Argument] string definitionPath)
+    {
+        var pipelineFile = new FileInfo(definitionPath);
+        if (!pipelineFile.Exists)
+        {
+            _ansiConsole.MarkupLine($"[red]Error:[/] Definition file '{definitionPath}' does not exist.");
+            return;
+        }
+
+        var pipelinesTask = pipelinesService
+            .GetLocalPipelinesAsync()
+            .ToListAsync()
+            .AsTask();
+
+        List<LocalPipelineInfo> pipelines = await _interactionService
+            .ShowStatusAsync("Loading...", () => pipelinesTask);
+
+        var thisPipeline = pipelines.FirstOrDefault(pipeline =>
+            pipeline.DefinitionFile.FullName.Equals(pipelineFile.FullName));
+
+        if (thisPipeline is null)
+        {
+            _ansiConsole.MarkupLine($"[red]Error:[/] No pipeline found for definition file '{definitionPath}'.");
+            return;
+        }
+
+        _ansiConsole.MarkupLine($"[blue]{thisPipeline.RelativePath}[/] refers to pipeline [bold green]{thisPipeline.Name}[/] [dim](ID: {thisPipeline.Id.Value})[/]");
+    }
+}
